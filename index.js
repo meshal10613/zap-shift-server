f89890becb19e7281c5f946feb7c3748019502ef
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-06-30 13:05:16 +0600

    pending, active, deactive rider done

--- index.js
+++ index.js
@@ -4,11 +4,40 @@ const cors = require("cors");
 const app = express();
 const port = process.env.port || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY)
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
+var admin = require("firebase-admin");
+const { initializeApp } = require('firebase-admin/app');
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//custom middleware
+const verifyFirebaseToken = async(req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).status({message: "unauthorized access"});
+    };
+    const token = authHeader.split(" ")[1];
+    if(!token){
+        res.status(401).status({message: "unauthorized access"});
+    };
+    //verify token
+    try{
+        const decocded = await admin.auth().verifyIdToken(token);
+        req.decocded = decocded;
+        next();
+    }catch(error){
+        return res.status(403).send({message: "forbidden access"});
+    }
+};
+
+const serviceAccount = require("./firebase-admin-key.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@meshal10613.mbbtx0s.mongodb.net/?retryWrites=true&w=majority&appName=meshal10613`;
 const YOUR_DOMAIN = 'http://localhost:5173';
@@ -38,9 +67,37 @@ async function run() {
         const parcelCollection = client.db("zap-shift").collection("parcels");
         const paymentsCollection = client.db("zap-shift").collection("payments");
         const trackingCollection = client.db("zap-shift").collection("payments");
+        const ridersCollection = client.db("zap-shift").collection("riders");
+
+        // userCollection
+        app.get("/users", async(req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+        app.post("/users", async(req, res) => {
+            const { email } = req.body;
+            //chech user already exist or not
+            const existUser = await userCollection.findOne({ email });
+            if(existUser){
+                const { lastSignInTime } = req.body;
+                const query = { email: email };
+                const updatedDoc = {
+                    $set: {
+                        lastSignInTime: lastSignInTime
+                    }
+                };
+                const result = await userCollection.updateOne(query, updatedDoc);
+                return res.status(200).send(result);
+            };
+
+            const user = req.body;
+            const result = await userCollection.insertOne(user);
+            res.status(201).send(result);
+        });
 
         // parcelCollection
-        app.get("/parcels", async(req, res) => {
+        app.get("/parcels",verifyFirebaseToken, async(req, res) => {
             try{
                 const {email} = req.query;
                 const query = email ? { created_by: email } : {};
@@ -86,14 +143,14 @@ async function run() {
         });
 
         //paymentsCollection
-        app.get('/payments', async (req, res) => {
+        app.get('/payments', verifyFirebaseToken, async (req, res) => {
 
             try {
                 const email = req.query.email;
-                // console.log('decocded', req.decoded)
-                // if (req.decoded.email !== userEmail) {
-                //     return res.status(403).send({ message: 'forbidden access' })
-                // }
+
+                if(req.decocded.email !== email) {
+                    return res.status(403).send({ message: 'forbidden access' })
+                }
 
                 const query = email ? { email: email } : {};
                 const options = { sort: { paid_at: -1 } }; // Latest first
@@ -179,6 +236,77 @@ async function run() {
             });
 
             res.json({clientSecret: session.client_secret});
+        });
+
+        //ridersCollection
+        app.get("/riders/pending", verifyFirebaseToken, async(req, res) => {
+            try {
+                const pendingRiders = await ridersCollection.find({ status: "pending" }).toArray();
+                res.send(pendingRiders);
+            } catch (error) {
+                console.error("Failed to load pending riders:", error);
+                res.status(500).send({ message: "Failed to load pending riders" });
+            }
+        });
+
+        app.get("/riders/active", verifyFirebaseToken, async(req, res) => {
+            try {
+                const activeRiders = await ridersCollection.find({ status: "active" }).toArray();
+                res.send(activeRiders);
+            } catch (error) {
+                console.error("Failed to load active riders:", error);
+                res.status(500).send({ message: "Failed to load active riders" });
+            }
+        });
+
+        app.get("/riders/deactivated", verifyFirebaseToken, async(req, res) => {
+            try {
+                const deactivateRiders = await ridersCollection.find({ status: "deactivated" }).toArray();
+                res.send(deactivateRiders);
+            } catch (error) {
+                console.error("Failed to load deactivated riders:", error);
+                res.status(500).send({ message: "Failed to load deactivated riders" });
+            }
+        });
+        
+        app.post('/riders', async(req, res) => {
+            const ridersData = req.body;
+            const result = await ridersCollection.insertOne(ridersData);
+            res.send(result);
+        });
+
+        app.patch('/riders/:id/status', async(req, res) => {
+            const { id } = req.params;
+            const { status, email } = req.body;
+            const query = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set:
+                {
+                    status
+                }
+            }
+
+            try {
+                const result = await ridersCollection.updateOne(
+                    query, updateDoc
+                );
+
+                // update user role for accepting rider
+                if (status === 'active') {
+                    const userQuery = { email };
+                    const userUpdateDoc = {
+                        $set: {
+                            role: 'rider'
+                        }
+                    };
+                    const roleResult = await usersCollection.updateOne(userQuery, userUpdateDoc)
+                    console.log(roleResult.modifiedCount)
+                }
+
+                res.send(result);
+            } catch (err) {
+                res.status(500).send({ message: "Failed to update rider status" });
+            }
         });
 
     } finally {
