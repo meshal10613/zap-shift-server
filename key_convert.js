fdeec3bbb41a6be978685810e8f0b0c4e72ad02e
Syed Mohiuddin Meshal <syedmohiuddinmeshal@gmail.com>
2025-07-03 21:09:16 +0600

    my earnings created

--- key_convert.js
+++ key_convert.js
@@ -1,4 +1,3 @@
 const fs = require('fs');
-const { buffer } = require('stream/consumers');
 const key = fs.readFileSync('./firebase-admin-key.json', 'utf8');
 const base64 = Buffer.from(key).toString('base64');
